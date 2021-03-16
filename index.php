92b50335f5ac3cbb5ce3e8a7c78220a573940d0c
willy0632080 <t0985012123@gmail.com>
2021-03-16 17:10:41 +0800

    add CSS setting

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <html>
 <title>劉乙賢的網站</title>
+<head>
+	<style>
+		body {
+  background-color: lightblue;
+		}
+
+		h1 {
+ 	 color: white;
+  	text-align: center;
+		}
+
+	p {
+  font-family: verdana;
+  font-size: 20px;
+	}
+	</style>
+</head>
 <body>
 
 <h1>歡迎光臨我的網站</h1>
-<p>This is a paragraph.</p>
+<p>失敗組</p>
 
 </body>
 </html>
